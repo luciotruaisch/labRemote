e55814892b27baa4819250a24a2af971a86382e1
Karol Krizka <kkrizka@gmail.com>
2017-12-18 14:07:05 -0800

    Implement Mojo communication protocol present in latest firmware.

--- src/libGPIB/MojoCom.cpp
+++ src/libGPIB/MojoCom.cpp
@@ -1,20 +1,24 @@
 #include "MojoCom.h"
 
 MojoCom::MojoCom(std::string dev) {
-    m_com = new SerialCom(dev, B115200);
-    // Reset
-    
-    char obuf[2] = {'\n', '\r'};
-    char ibuf[2] = {' ', ' '};
-    for (unsigned n=0; n<10; n++) {
-        m_com->write(obuf, 2);
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-        for (unsigned i=0; i<2; i+=m_com->read(&ibuf[i], 2-i))
-            std::this_thread::sleep_for(std::chrono::milliseconds(1));
-        if (ibuf[1] != '\n') {
-            log(logERROR) << __PRETTY_FUNCTION__ << " : Reset failed!";
-        }
+  m_com = new SerialCom(dev, B115200);
+  // Reset
+
+  char obuf[] = "\r\n";
+  char ibuf[2];
+  for (unsigned n=0; n<10; n++) {
+    m_com->write(obuf, 2);
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    for (unsigned i=0; i<2; i+=m_com->read(&ibuf[i], 2-i))
+      std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
+    if (ibuf[1] != '\n') {
+      log(logERROR) << __PRETTY_FUNCTION__ << " : Reset failed!";
+    } else {
+      break;
     }
+  }
 }
 
 MojoCom::~MojoCom() {
@@ -22,80 +26,65 @@ MojoCom::~MojoCom() {
 }
 
 int MojoCom::writeReg(unsigned reg, unsigned val) {
-    log(logDEBUG1) << __PRETTY_FUNCTION__ << " : reg(" << reg << ") val(" << val << ")";
-    char outbuf[7], inbuf[4];
+  log(logDEBUG1) << __PRETTY_FUNCTION__ << " : reg(" << reg << ") val(" << val << ")";
+  char outbuf[7], inbuf[4];
 
-    // Prepare buffer
-    outbuf[0] = 0x80 | ((reg >> 8) & 0xFF);
-    outbuf[1] = reg & 0xFF;
-    outbuf[2] = (val >> 24 ) & 0xFF;
-    outbuf[3] = (val >> 16 ) & 0xFF;
-    outbuf[4] = (val >> 8 ) & 0xFF;
-    outbuf[5] = val & 0xFF;
-    outbuf[6] = '\n';
+  // Prepare buffer
+  outbuf[0] = 0x80 | ((reg >> 8) & 0xFF);
+  outbuf[1] = reg & 0xFF;
+  outbuf[2] = (val >> 24 ) & 0xFF;
+  outbuf[3] = (val >> 16 ) & 0xFF;
+  outbuf[4] = (val >> 8 ) & 0xFF;
+  outbuf[5] = val & 0xFF;
+  outbuf[6] = '\n';
 
-    // Write buffer
-    if (m_com->write(outbuf, 7) < 7) {
-        log(logERROR) << __PRETTY_FUNCTION__ << " : Failed writing all bytes!";
-        return -1;
-    }
-    // Read status
-    
-    std::this_thread::sleep_for(std::chrono::milliseconds(1));
-    for (unsigned i=0; i<2; i+=m_com->read(&inbuf[i], 2-i))
-        std::this_thread::sleep_for(std::chrono::milliseconds(1));
-    /*
-    if (m_com->read(inbuf, 2) < 2) {
-        log(logERROR) << __PRETTY_FUNCTION__ << " : Failed reading status bytes!";
-        return -1;
-    }
-    */
-    // Check status
-    if (((inbuf[0] & 0xF) > 0) || (inbuf[1] != '\n')) {
-        log(logERROR) << __PRETTY_FUNCTION__<< " : Status signals error!";
-        return -1;
-    }
+  // Write buffer
+  if (m_com->write(outbuf, 7) < 7) {
+    log(logERROR) << __PRETTY_FUNCTION__ << " : Failed writing all bytes!";
+    return -1;
+  }
 
-    return 0;
+  return 0;
 }
 
 int MojoCom::readReg(unsigned reg, unsigned &val) {
-    log(logDEBUG1) << __PRETTY_FUNCTION__ << " : reg(" << reg << ")";
-    char outbuf[3], inbuf[8];
+  log(logDEBUG1) << __PRETTY_FUNCTION__ << " : reg(" << reg << ")";
+  char outbuf[3], inbuf[6];
 
-    // Prepare output
-    outbuf[0] = 0x00 | ((reg >> 8) & 0xFF);
-	outbuf[1] = reg & 0xFF;
-	outbuf[2] = '\n';
+  // Prepare output
+  outbuf[0] = 0x00 | ((reg >> 8) & 0xFF);
+  outbuf[1] = reg & 0xFF;
+  outbuf[2] = '\n';
 
-    // Write output
-    if (m_com->write(outbuf, 3) < 3) return -1;
-    // Read status
-    std::this_thread::sleep_for(std::chrono::milliseconds(1));
-    for (unsigned i=0; i<2; i+=m_com->read(&inbuf[i], 2-i))
-        std::this_thread::sleep_for(std::chrono::milliseconds(1));
-    
-    //if (m_com->read(inbuf, 2) < 2) return -1;
-    // Check status
-    if (!(inbuf[0] & 0x8)) return -1;
-   
-    // Read data
-    for (unsigned i=0; i<4; i+=m_com->read(&inbuf[2+i], 4-i))
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
-    //if (m_com->read(&inbuf[2], 4) < 4) return -1;
-    // Check status
-    if (inbuf[5] != '\n') return -1;
-    // Trasnfer data
-    for(int i = 1; i < 5; i++) val = (val<<8) + (unsigned char)inbuf[i];
-    log(logDEBUG1) << __PRETTY_FUNCTION__ << " : val(" << val << ")";
-    return 0;
+  // Write output
+  if (m_com->write(outbuf, 3) < 3) 
+    {
+      log(logERROR) << __PRETTY_FUNCTION__ << " : Failed writing all bytes!";
+      return -1;
+    }
+
+  // Read data
+  for (unsigned i=0; i<6; i+=m_com->read(&inbuf[i], 6-i))
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+  // Check status
+  if (inbuf[5] != '\n')
+    {
+      log(logERROR) << __PRETTY_FUNCTION__ << " : Wrong read response!";
+      return -1;
+    }
+
+  // Trasnfer data
+  for(int i = 1; i < 5; i++) val = (val<<8) + (unsigned char)inbuf[i];
+  log(logDEBUG1) << __PRETTY_FUNCTION__ << " : val(" << val << ")";
+  return 0;
 }
 
 int MojoCom::enableI2C() {
     log(logINFO) << __PRETTY_FUNCTION__ << " : Enabling I2C!";
     // Reset I2C
-    if (this->writeReg(0x4c, 0x0))  return -1;
-    if (this->writeReg(0x4c, 0x03)) return -1; // unreset I2C + drive SCL active
+    if (this->writeReg(0x4c, 0x00))  return -1;
+    if (this->writeReg(0x4c, 0x01)) return -1; // unreset I2C + drive SCL active
 
     // Set the I2C clock prescale
     if (this->writeReg(0x100, 0x00)) return -1;
@@ -109,105 +98,105 @@ int MojoCom::enableI2C() {
 int MojoCom::readI2C(unsigned id, unsigned addr, unsigned *data, unsigned bytes) {
     unsigned i(0), reg(0);
 
-	// Write Slave Address(RW = W) + Start Condition
-	if(writeReg(0x103, id<<1)) return -1;
-	if(writeReg(0x104,0x90)) return -1;
-	do{
-		usleep(1000);
-		if(readReg(0x104, reg)) return -1;
-	}while(reg & 0x02);
-	if(reg != 0x41)
-		return -1;
+    // Write Slave Address(RW = W) + Start Condition
+    if(writeReg(0x103, id<<1)) return -1;
+    if(writeReg(0x104,0x90)) return -1;
+    do{
+      usleep(1000);
+      if(readReg(0x104, reg)) return -1;
+    }while(reg & 0x02);
+    if(reg != 0x41)
+      return -1;
 
-	// Write Register Address
-	if(writeReg(0x103, addr)) return -1;
-	if(writeReg(0x104,0x10)) return -1;
-	do{
-		usleep(1000);
-		if(readReg(0x104, reg)) return -1;
-	}while(reg & 0x02);
-	if(reg != 0x41)
-		return -1;
-
-	// Write Slave Address(RW = R) + Repeated Start Condition
-	if(writeReg(0x103, id*2+1)) return -1;
-	if(writeReg(0x104,0x90)) return -1;
-	do{
-		usleep(1000);
-		if(readReg(0x104, reg)) return -1;
-	}while(reg & 0x02);
-	if(reg != 0x41)
-		return -1;
-
-	// Read Bytes
-	for(i = 0; i < (bytes - 1); i++){;
-		if(writeReg(0x104,0x20)) return -1;
-		do{
-			usleep(1000);
-			if(readReg(0x104, reg)) return -1;
-		}while(reg & 0x02);
-		if(reg != 0x41)
-			return -1;
-		if(readReg(0x103, data[i])) return -1;
-	}
-
-	// Read last byte + Stop Condition & NACK
-	if(writeReg(0x104,0x68)) return -1;
-	do{
-		usleep(1000);
-		if(readReg(0x104, reg)) return -1;
-	}while(reg & 0x02);
-	if(reg != 0x81)
-		return -1;
-	if(readReg(0x103, data[i])) return -1;
-
-	return 0;
-}
-
-int MojoCom::writeI2C(unsigned id, unsigned addr, unsigned *data, unsigned bytes) {
-	unsigned i(0), reg(0);
-
-    // Write id + Start Condition
-	if(writeReg(0x103, id<<1)) return -1;
-	if(writeReg(0x104,0x90)) return -1;
-	do{
-		usleep(1000);
-		if(readReg(0x104, reg)) return -1;
-	}while(reg & 0x02);
-	if(reg != 0x41)
-		return -1;
-
-    // Write reg addr
+    // Write Register Address
     if(writeReg(0x103, addr)) return -1;
     if(writeReg(0x104,0x10)) return -1;
     do{
-        usleep(1000);
-        if(readReg(0x104, reg)) return -1;
+      usleep(1000);
+      if(readReg(0x104, reg)) return -1;
     }while(reg & 0x02);
     if(reg != 0x41)
-        return -1;
+      return -1;
 
-	// Write Bytes
-	for(i = 0; i < (bytes-1); i++){
-		if(writeReg(0x103, data[i])) return -1;
-		if(writeReg(0x104,0x10)) return -1;
-		do{
-			usleep(1000);
-			if(readReg(0x104, reg)) return -1;
-		}while(reg & 0x02);
-		if(reg != 0x41)
-			return -1;
-	}
+    // Write Slave Address(RW = R) + Repeated Start Condition
+    if(writeReg(0x103, id*2+1)) return -1;
+    if(writeReg(0x104,0x90)) return -1;
+    do{
+      usleep(1000);
+      if(readReg(0x104, reg)) return -1;
+    }while(reg & 0x02);
+    if(reg != 0x41)
+      return -1;
+
+    // Read Bytes
+    for(i = 0; i < (bytes - 1); i++){;
+      if(writeReg(0x104,0x20)) return -1;
+      do{
+	usleep(1000);
+	if(readReg(0x104, reg)) return -1;
+      }while(reg & 0x02);
+      if(reg != 0x41)
+	return -1;
+      if(readReg(0x103, data[i])) return -1;
+    }
+
+    // Read last byte + Stop Condition & NACK
+    if(writeReg(0x104,0x68)) return -1;
+    do{
+      usleep(1000);
+      if(readReg(0x104, reg)) return -1;
+    }while(reg & 0x02);
+    if(reg != 0x81)
+      return -1;
+    if(readReg(0x103, data[i])) return -1;
+
+    return 0;
+}
+
+int MojoCom::writeI2C(unsigned id, unsigned addr, unsigned *data, unsigned bytes) {
+  unsigned i(0), reg(0);
+
+  // Write id + Start Condition
+  if(writeReg(0x103, id<<1)) return -1;
+  if(writeReg(0x104,0x90)) return -1;
+  do{
+    usleep(1000);
+    if(readReg(0x104, reg)) return -1;
+  }while(reg & 0x02);
+  if(reg != 0x41)
+    return -1;
+
+  // Write reg addr
+  if(writeReg(0x103, addr)) return -1;
+  if(writeReg(0x104,0x10)) return -1;
+  do{
+    usleep(1000);
+    if(readReg(0x104, reg)) return -1;
+  }while(reg & 0x02);
+  if(reg != 0x41)
+    return -1;
+
+  // Write Bytes
+  for(i = 0; i < (bytes-1); i++){
+    if(writeReg(0x103, data[i])) return -1;
+    if(writeReg(0x104,0x10)) return -1;
+    do{
+      usleep(1000);
+      if(readReg(0x104, reg)) return -1;
+    }while(reg & 0x02);
+    if(reg != 0x41)
+      return -1;
+  }
 	
-	// Write Byte + Stop Condition
-	if(writeReg(0x103, data[i])) return -1;
-	if(writeReg(0x104,0x50)) return -1;
-	do{
-		usleep(1000);
-		if(readReg(0x104, reg)) return -1;
-	}while(reg & 0x02);
-	if(reg != 0x01)
-		return -1;
+  // Write Byte + Stop Condition
+  if(writeReg(0x103, data[i])) return -1;
+  if(writeReg(0x104,0x50)) return -1;
+  do{
+    usleep(1000);
+    if(readReg(0x104, reg)) return -1;
+  }while(reg & 0x02);
+  if(reg != 0x01)
+    return -1;
 
-	return 0;
+  return 0;
 }
