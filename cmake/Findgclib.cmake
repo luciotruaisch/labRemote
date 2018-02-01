b1e4a2105f2287163de4da8626f7b4b4625f725e
Karol Krizka <karol.krizka@cern.ch>
2018-02-01 22:54:05 +0000

    Fix libGPIB linking to libmpsse when libmpsse is not available. Add nonfree…

---
+++ cmake/Findgclib.cmake
@@ -0,0 +1,23 @@
+# - Try to find libmpsse
+# Once done this will define
+#  gclib_FOUND - System has libmpsse
+#  gclib_INCLUDE_DIRS - The libmpsse include directories
+#  gclib_LIBRARIES - The libraries needed to use libmpsse
+#  gclib_DEFINITIONS - Compiler switches required for using libmpsse
+
+FIND_PATH(gclib_INCLUDE_DIR gclib.h
+          HINTS /usr/include)
+
+FIND_LIBRARY(gclib_LIBRARY NAMES gclib libgclib
+              HINTS /usr/lib64 )
+
+INCLUDE(FindPackageHandleStandardArgs)
+# handle the QUIETLY and REQUIRED arguments and set gclib_FOUND to TRUE
+# if all listed variables are TRUE
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(gclib DEFAULT_MSG
+  gclib_LIBRARY gclib_INCLUDE_DIR)
+
+MARK_AS_ADVANCED(gclib_INCLUDE_DIR gclib_LIBRARY )
+
+SET(gclib_LIBRARIES ${gclib_LIBRARY} )
+SET(gclib_INCLUDE_DIRS ${gclib_INCLUDE_DIR} )
--- src/libImageRec/OpenCVHelper.cpp
+++ src/libImageRec/OpenCVHelper.cpp
@@ -1,9 +1,9 @@
 #include "OpenCVHelper.h"
 
-#include "opencv2/core.hpp"
-#include "opencv2/imgproc.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/features2d.hpp"
+#include "opencv2/core/core.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/features2d/features2d.hpp"
 #include "opencv2/xfeatures2d.hpp"
 
 #include "opencv2/calib3d/calib3d.hpp" // for findHomography
