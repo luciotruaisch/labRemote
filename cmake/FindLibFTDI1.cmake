cbaf44b1503de6f4de0999dd1d39e09234d4b67f
ssantpur <sai.neha.santpur@cern.ch>
2018-09-17 15:08:26 +0100

    TTI PS code ready

--- cmake/FindLibFTDI1.cmake
+++ cmake/FindLibFTDI1.cmake
@@ -8,8 +8,8 @@
 FIND_PATH(LIBFTDI_INCLUDE_DIR ftdi.h
   HINTS /usr/include/libftdi1 )
 
-FIND_LIBRARY(LIBFTDI_LIBRARY NAMES ftdi1 libftdi1
-  HINTS /usr/lib64 )
+FIND_LIBRARY(LIBFTDI_LIBRARY NAMES ftdi1 libftdi1 libftdi
+  HINTS /usr/lib/x86_64-linux-gnu )
 
 INCLUDE(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set LIBFTDI_FOUND to TRUE
