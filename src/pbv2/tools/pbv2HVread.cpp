419bc90bac43b559974904a0d3d893b6854c018f
luciotruaisch <lucio.truaisch@gmail.com>
2019-07-06 15:30:52 +0000

    Tested ADC

--- src/pbv2/tools/pbv2HVread.cpp
+++ src/pbv2/tools/pbv2HVread.cpp
@@ -1,27 +1,38 @@
 #include <string>
 #include <iostream>
 #include <thread>
+#include <fstream>
 
 #include "PBv2TB.h"
+#include "SorensenPs.h"
 
 int main(){
   
   PBv2TB tb;
-  
-  tb.setHVamp(0,1);
-  
-  std::this_thread::sleep_for(std::chrono::seconds(5));
+  SorensenPs ps("/dev/ttyUSB0",11);
+  ps.setCh(2);
+  ps.init();
+  ps.setCh(2);
 
-  uint8_t Gain = 32;
-  uint8_t PB = 32;
-  tb.getHVamp(PB,Gain);
-  std::cout << "Gain is:"<< (unsigned)Gain << "   Channel is:" << (unsigned)PB<< std::endl;
+  double Vmin = 0.1;
+  double Vmax = 3.3;
+  //Creat a test file
+  std::ofstream TestFile;
+  TestFile.open("HV_ADCread.csv");
+  TestFile << "HVsupply,HVmes"<<std::endl;
 
-  while(1)
+
+
+  for(double V=0.0;V<Vmax;V+=Vmin)
     {
+      ps.setVoltage(V);
+      ps.turnOn();
+      std::this_thread::sleep_for(std::chrono::seconds(2));
       double HV = tb.getHVout();
-      std::this_thread::sleep_for(std::chrono::seconds(5));
-      std::cout << "HV measured:" << HV << std::endl;
-    }
+      double HV_supply = std::stod(ps.getVoltage());
 
+      TestFile << HV_supply <<","<<HV<<std::endl;
+      ps.turnOff();
+    }
+  TestFile.close();
 }
